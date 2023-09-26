3f7d4225cc1c11e52cd18a178850eb4883a63f22
Nwaigba66 <glorianwaigba@gmail.com>
2023-09-26 03:24:33 -0700

    updated codebase

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,44 +13,29 @@ def export_todos_to_csv(employee_id):
     """
     id_endpoint = "https://jsonplaceholder.typicode.com/users/" + employee_id
     todos_endpoint = id_endpoint + "/todos"
-
     try:
-        # Get user's data
         response = requests.get(id_endpoint)
         user_data = response.json()
         username = user_data.get('username')
-
-        # Get tasks data
         response = requests.get(todos_endpoint)
         todos = response.json()
 
         # Create a CSV file
         csv_filename = employee_id + ".csv"
         with open(csv_filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_NONNUMERIC)
-
-            # Write todo rows
             for todo in todos:
                 task_completed_status = str(todo.get('completed'))
                 task_title = todo.get('title')
-                csv_writer.writerow([employee_id, username,
-                                    task_completed_status, task_title])
-
-        print("Data has been exported to " + csv_filename)
-
+                csv_file.write('"{}","{}","{}","{}"\n'.format(
+                    user_data.get("id"),
+                    username,
+                    task_completed_status,
+                    task_title))
     except Exception:
         print("Something went wrong.")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
     # check if input is an integer
     employee_id = sys.argv[1]
-    if not employee_id.isdigit():
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
     export_todos_to_csv(employee_id)
