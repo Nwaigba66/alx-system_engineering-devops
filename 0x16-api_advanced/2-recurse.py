d8396786c8b4a4546f394b89c91017d46013f9f1
Nwaigba66 <glorianwaigba@gmail.com>
2023-10-11 05:01:01 +0000

    Updated codebase

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-""" return list contaning the title of hot articles
-for a given subreddit """
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """ reuturns all hot articles"""
-    endpoint = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16-api_advanced.0-sub.py:\
-        v1.0.0 (by /u/thomaskitaba)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
-            "limit": 100,
-            "after": after,
-            "count": count
-            }
-    response = requests.get(endpoint, headers=headers,
-                            params=params, allow_redirects=False)
-
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
     if response.status_code == 404:
         return None
 
-    data = response.json().get("data")
-    children = data.get("children")
-    after = data.get("after")
-    count += int(data.get("dist"))
-    for post in children:
-        hot_list.append(post.get("data").get("title"))
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
-        recurse(subreddit, hot_list, after, count)
+        return recurse(subreddit, hot_list, after, count)
     return hot_list
